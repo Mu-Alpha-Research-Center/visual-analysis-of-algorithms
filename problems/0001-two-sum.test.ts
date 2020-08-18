461ea22165da3502d5a5ea2a77f255ee779a3701
hachibu <raymondsohn@gmail.com>
2020-08-18 09:34:34 -0700

    Move to TypeScript

---
+++ problems/0001-two-sum.test.ts
@@ -0,0 +1,19 @@
+var twoSum = function(nums, target) {
+};
+
+test('twoSum', () => {
+//  expect(runningSum([1, 2, 3, 4])).toStrictEqual([1, 3, 6, 10]);
+//  expect(runningSum([1, 1, 1, 1, 1])).toStrictEqual([1, 2, 3, 4, 5]);
+});
+
+//
+//
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+//Given nums = [2, 7, 11, 15], target = 9,
+//
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+//
---
+++ problems/1480-running-sum.test.ts
@@ -0,0 +1,16 @@
+function runningSum(nums: number[]): number[] {
+  var result = []
+  for (let i = 0; i < nums.length; i++) {
+    let sum = nums[i]
+    for (let j = 0; j < i; j++) {
+      sum += nums[j];
+    }
+    result.push(sum)
+  }
+  return result
+};
+
+test('runningSum', () => {
+  expect(runningSum([1, 2, 3, 4])).toStrictEqual([1, 3, 6, 10])
+  expect(runningSum([1, 1, 1, 1, 1])).toStrictEqual([1, 2, 3, 4, 5])
+})
