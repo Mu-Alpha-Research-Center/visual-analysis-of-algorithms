5e7bb999c0833484a18912503f003699fc931c9e
hachibu <raymondsohn@gmail.com>
2020-09-01 13:49:42 -0700

    Solve problem 13

---
+++ src/0013-roman-to-integer.test.ts
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/roman-to-integer
+
+const RomanNumeral: { [key: string]: number } = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+}
+
+function romanToInt(s: string): number {
+  let result: number = 0
+  for (let i = 0; i < s.length; i++) {
+    let curr:number = RomanNumeral[s[i]],
+        next:number = RomanNumeral[s[i + 1]]
+    result += next > curr ? -curr : curr
+  }
+  return result
+}
+
+const testEach = test.each([
+  ['III', 3],
+  ['IV', 4],
+  ['IX', 9],
+  ['LVIII', 58],
+  ['MCMXCIV', 1994],
+])
+
+testEach('romanToInt(%p)', (a, expected) => expect(romanToInt(a)).toBe(expected))
