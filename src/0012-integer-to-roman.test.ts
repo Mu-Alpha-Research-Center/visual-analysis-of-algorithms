d39f111b8213e26965b80ab6c285fee491f79a17
hachibu <raymondsohn@gmail.com>
2020-09-02 12:00:17 -0700

    Solve problem 12

--- src/0012-integer-to-roman.test.ts
+++ src/0012-integer-to-roman.test.ts
@@ -1,26 +1,38 @@
 // https://leetcode.com/problems/integer-to-roman
 
-let RomanNumeral: { [key: string]: number } = {
-  M: 1000,
-  D: 500,
-  C: 100,
-  L: 50,
-  X: 10,
-  V: 5,
-  I: 1,
-}
-
 function intToRoman(num: number): string {
   let result: string = ''
+  let romanNumerals: { [key: string]: number } = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  }
+
+  for (let [romanNumeral, value] of Object.entries(romanNumerals)) {
+    while (num >= value) {
+      num -= value
+      result += romanNumeral
+    }
+  }
 
   return result
 }
 
 let testEach = test.each([
-  //[3, 'III'],
-  //[4, 'IV'],
-  //[9, 'IX'],
-  //[58, 'LVIII'],
+  [3, 'III'],
+  [4, 'IV'],
+  [9, 'IX'],
+  [58, 'LVIII'],
   [1994, 'MCMXCIV'],
 ])
 
