04d78445a11aacc2b82447207ecd0bc5d36996fb
hachibu <raymondsohn@gmail.com>
2020-12-12 20:36:35 -0800

    Solve problem 17 with brute force

--- src/0017-letter-combinations-of-a-phone-number.test.ts
+++ src/0017-letter-combinations-of-a-phone-number.test.ts
@@ -1,13 +1,18 @@
 // https://leetcode.com/problems/letter-combinations-of-a-phone-number
 // https://www.geeksforgeeks.org/permutation-and-combination
 
-import { runTests, todo } from './TestHelpers'
+import { runTests } from './TestHelpers'
 
 function digitsToLetterGroups(digits: string): string[][] {
-  let letters = {
-    2: 'abc', 3: 'def', 4: 'ghi',
-    5: 'jkl', 6: 'mno', 7: 'pqrs',
-    8: 'tuv', 9: 'wxyz'
+  let letters: Record<number, string> = {
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz'
   }
   return digits.split('').map(digit => letters[digit].split(''))
 }
@@ -15,20 +20,43 @@ function digitsToLetterGroups(digits: string): string[][] {
 function letterCombinations(digits: string): string[] {
   let result: string[] = []
   let groups: string[][] = digitsToLetterGroups(digits)
-  let letters: string[] = groups.shift()
 
-  if (groups.length === 0) {
-    return result
-  } else if (groups.length === 1) {
-    return groups.pop()
+  if (groups.length === 1) {
+    for (let a of groups[0]) {
+      result.push(a)
+    }
+  } else if (groups.length === 2) {
+    for (let a of groups[0]) {
+      for (let b of groups[1]) {
+        result.push(a + b)
+      }
+    }
+  } else if (groups.length === 3) {
+    for (let a of groups[0]) {
+      for (let b of groups[1]) {
+        for (let c of groups[2]) {
+          result.push(a + b + c)
+        }
+      }
+    }
+  } else if (groups.length === 4) {
+    for (let a of groups[0]) {
+      for (let b of groups[1]) {
+        for (let c of groups[2]) {
+          for (let d of groups[3]) {
+            result.push(a + b + c + d)
+          }
+        }
+      }
+    }
   }
 
   return result
 }
 
 runTests(letterCombinations, [
-  todo(['', []]),
-  todo(['2', ['a', 'b', 'c']]),
-  todo(['23', ['ad','ae','af','bd','be','bf','cd','ce','cf']]),
-  todo(['234', ['adg','adh','adi','aeg','aeh','aei','afg','afh','afi','bdg','bdh','bdi','beg','beh','bei','bfg','bfh','bfi','cdg','cdh','cdi','ceg','ceh','cei','cfg','cfh','cfi']])
+  ['', []],
+  ['2', ['a', 'b', 'c']],
+  ['23', ['ad','ae','af','bd','be','bf','cd','ce','cf']],
+  ['234', ['adg','adh','adi','aeg','aeh','aei','afg','afh','afi','bdg','bdh','bdi','beg','beh','bei','bfg','bfh','bfi','cdg','cdh','cdi','ceg','ceh','cei','cfg','cfh','cfi']]
 ])
