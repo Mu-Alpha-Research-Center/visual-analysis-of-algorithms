c66c5a52249e5114a5000a321837c07ef91e40a8
hachibu <raymondsohn@gmail.com>
2021-01-19 06:01:16 -0800

    Solve problem 1281

--- src/problems/1281-subtract-the-product-and-sum-of-digits-of-an-integer.test.ts
+++ src/problems/1281-subtract-the-product-and-sum-of-digits-of-an-integer.test.ts
@@ -3,13 +3,22 @@
 import Tests from '../TestHelpers'
 
 function subtractProductAndSum(n: number): number {
-  return 15
+  let product = 1
+  let sum = 0
+  while (n > 0) {
+    let lastDigit = n % 10
+    product *= lastDigit
+    sum += lastDigit
+    n = Math.floor(n / 10)
+  }
+  // The time complexity is order of n.
+  // The space complexity is order of 1.
+  return product - sum
 }
 
 let tests = new Tests(
-  [234, 15]
+  [234, 15],
+  [4421, 21]
 )
 
-tests.run(
-  subtractProductAndSum
-)
+tests.run(subtractProductAndSum)
