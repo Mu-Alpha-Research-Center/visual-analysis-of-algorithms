545dc2b88d5c75b7e726a72948027d7954881976
hachibu <raymondsohn@gmail.com>
2021-01-23 15:46:28 -0800

    Add TreeNode and tests

---
+++ src/DataStructures/TreeNode.test.ts
@@ -0,0 +1,51 @@
+import TreeNode from './TreeNode'
+
+test('breadth-first search', () => {
+  let root = new TreeNode('a', [
+    new TreeNode('b', [
+      new TreeNode('d'),
+      new TreeNode('e')
+    ]),
+    new TreeNode('c')
+  ])
+
+  expect(root.bfs(({ value }) => value === 'e').value).toStrictEqual('e')
+  expect(root.bfs(({ value }) => value === 'f')).toStrictEqual(null)
+})
+
+test('breadth-first traverse', () => {
+  let root = new TreeNode('a', [
+    new TreeNode('b', [
+      new TreeNode('d'),
+      new TreeNode('e')
+    ]),
+    new TreeNode('c')
+  ])
+
+  expect(root.bft().map(node => node.value)).toStrictEqual(['a', 'b', 'c', 'd', 'e'])
+})
+
+test('depth-first search', () => {
+  let root = new TreeNode('a', [
+    new TreeNode('b', [
+      new TreeNode('c'),
+      new TreeNode('d')
+    ]),
+    new TreeNode('e')
+  ])
+
+  expect(root.dfs(({ value }) => value === 'e').value).toStrictEqual('e')
+  expect(root.dfs(({ value }) => value === 'f')).toStrictEqual(null)
+})
+
+test('depth-first traverse', () => {
+  let root = new TreeNode('a', [
+    new TreeNode('b', [
+      new TreeNode('c'),
+      new TreeNode('d')
+    ]),
+    new TreeNode('e')
+  ])
+
+  expect(root.dft().map(node => node.value)).toStrictEqual(['a', 'b', 'c', 'd', 'e'])
+})
---
+++ src/DataStructures/TreeNode.ts
@@ -0,0 +1,62 @@
+type Predicate<T> = (node: TreeNode<T>) => boolean
+
+export default class TreeNode<T> {
+  value: T
+  children: TreeNode<T>[]
+
+  constructor(value: T, children: TreeNode<T>[] = []) {
+    this.value = value
+    this.children = children
+  }
+
+  bfs(pred: Predicate<T>): TreeNode<T> | null {
+    let queue = []
+    queue.push(this)
+    while (queue.length > 0) {
+      let node = queue.shift()
+      if (pred(node)) {
+        return node
+      }
+      for (let child of node.children) {
+        queue.push(child)
+      }
+    }
+    return null
+  }
+
+  bft(): TreeNode<T>[] {
+    let nodes = []
+    let queue = []
+    queue.push(this)
+    while (queue.length > 0) {
+      let node = queue.shift()
+      nodes.push(node)
+      for (let child of node.children) {
+        queue.push(child)
+      }
+    }
+    return nodes
+  }
+
+  dfs(pred: Predicate<T>): TreeNode<T> | null {
+    if (pred(this)) {
+      return this
+    }
+    for (let child of this.children) {
+      let node = child.dfs(pred)
+      if (node) {
+        return node
+      }
+    }
+    return null
+  }
+
+  dft(): TreeNode<T>[] {
+    let nodes = []
+    nodes.push(this)
+    for (let child of this.children) {
+      nodes = nodes.concat(child.dft())
+    }
+    return nodes
+  }
+}
