b7b673c3a0167ed3aec923bde75366553433d838
Raymond Sohn <raymondsohn@gmail.com>
2023-09-23 21:07:25 -0700

    Implement linked list search

--- src/the-algorithm-design-manual/data-structures/LinkedList.test.ts
+++ src/the-algorithm-design-manual/data-structures/LinkedList.test.ts
@@ -7,6 +7,7 @@ class LinkedListNode<T> {
 
 interface ILinkedList<T> {
     insert(item: T): void
+    search(item: T): LinkedListNode<T> | null
 }
 
 class LinkedList<T> implements ILinkedList<T> {
@@ -18,6 +19,17 @@ class LinkedList<T> implements ILinkedList<T> {
         } else {
             this.head = new LinkedListNode(item, this.head)
         }
+    }
+
+    public search(item: T): LinkedListNode<T> | null {
+        let curr = this.head
+        while (curr) {
+            if (curr.item === item) {
+                return curr
+            }
+            curr = curr.next
+        }
+        return null
     }
 }
 
@@ -34,4 +46,16 @@ test('insert', () => {
             next: { item: 2, next: { item: 3, next: undefined } },
         },
     })
+})
+
+test('search', () => {
+    const list = new LinkedList()
+
+    list.insert(3)
+    list.insert(2)
+    list.insert(1)
+
+    const item = list.search(3)
+
+    expect(item).toEqual({ item: 3, next: undefined })
 })
