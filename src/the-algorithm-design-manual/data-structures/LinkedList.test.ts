4501de0eae3e17105fb32e6dc6b7f81b9f53aacf
Raymond Sohn <raymondsohn@gmail.com>
2023-09-23 21:17:07 -0700

    Implement linked list delete

--- src/the-algorithm-design-manual/data-structures/LinkedList.test.ts
+++ src/the-algorithm-design-manual/data-structures/LinkedList.test.ts
@@ -8,10 +8,17 @@ class LinkedListNode<T> {
 interface ILinkedList<T> {
     insert(item: T): void
     search(item: T): LinkedListNode<T> | null
+    delete(item: T): void
 }
 
 class LinkedList<T> implements ILinkedList<T> {
-    constructor(private head?: LinkedListNode<T>) {}
+    public head?: LinkedListNode<T>
+
+    constructor(...items: T[]) {
+        for (const item of items.reverse()) {
+            this.insert(item)
+        }
+    }
 
     public insert(item: T) {
         if (this.head === null) {
@@ -31,6 +38,19 @@ class LinkedList<T> implements ILinkedList<T> {
         }
         return null
     }
+
+    public delete(item: T) {
+        let prev = null
+        let curr = this.head
+        while (curr) {
+            if (curr.item === item) {
+                break
+            }
+            prev = curr
+            curr = curr.next
+        }
+        prev.next = curr.next
+    }
 }
 
 test('insert', () => {
@@ -40,22 +60,23 @@ test('insert', () => {
     list.insert(2)
     list.insert(1)
 
-    expect(list).toEqual({
-        head: {
-            item: 1,
-            next: { item: 2, next: { item: 3, next: undefined } },
-        },
+    expect(list.head).toEqual({
+        item: 1,
+        next: { item: 2, next: { item: 3 } },
     })
 })
 
 test('search', () => {
-    const list = new LinkedList()
-
-    list.insert(3)
-    list.insert(2)
-    list.insert(1)
-
+    const list = new LinkedList(1, 2, 3)
     const item = list.search(3)
 
-    expect(item).toEqual({ item: 3, next: undefined })
+    expect(item).toEqual({ item: 3 })
+})
+
+test('delete', () => {
+    const list = new LinkedList(1, 2, 3)
+
+    list.delete(2)
+
+    expect(list.head).toEqual({ item: 1, next: { item: 3 } })
 })
