a75ac71dabf289909016438b77046c484a25c43a
Raymond Sohn <raymondsohn@gmail.com>
2023-12-30 19:18:12 -0800

    Update API for array binary tree

--- src/data_structures/test_array_binary_tree.py
+++ src/data_structures/test_array_binary_tree.py
@@ -5,21 +5,21 @@ class ArrayBinaryTree:
     def index_out_of_range(self, i: int) -> bool:
         return i < 0 or i > len(self.array) - 1
 
-    def get_parent(self, n):
+    def parent(self, n):
         i = self.array.index(n)
         j = (i - 1) // 2
         if self.index_out_of_range(j):
             return None
         return self.array[j]
 
-    def get_left_child(self, n):
+    def left_child(self, n):
         i = self.array.index(n)
         j = 2 * i + 1
         if self.index_out_of_range(j):
             return None
         return self.array[j]
 
-    def get_right_child(self, n):
+    def right_child(self, n):
         i = self.array.index(n)
         j = 2 * i + 2
         if self.index_out_of_range(j):
@@ -54,19 +54,19 @@ def test_getters():
     # 1   3
     tree = ArrayBinaryTree([4, 2, 5, 1, 3])
 
-    get_parent_tests = [
+    parent_tests = [
         (4, None),
         (2, 4),
         (5, 4),
         (1, 2),
         (3, 2),
     ]
-    get_left_child_tests = [(4, 2), (2, 1), (1, None), (3, None), (5, None)]
-    get_right_child_tests = [(4, 5), (5, None), (2, 3), (3, None), (1, None)]
+    left_child_tests = [(4, 2), (2, 1), (1, None), (3, None), (5, None)]
+    right_child_tests = [(4, 5), (5, None), (2, 3), (3, None), (1, None)]
 
-    for n, expected in get_parent_tests:
-        assert tree.get_parent(n) == expected
-    for n, expected in get_left_child_tests:
-        assert tree.get_left_child(n) == expected
-    for n, expected in get_right_child_tests:
-        assert tree.get_right_child(n) == expected
+    for n, expected in parent_tests:
+        assert tree.parent(n) == expected
+    for n, expected in left_child_tests:
+        assert tree.left_child(n) == expected
+    for n, expected in right_child_tests:
+        assert tree.right_child(n) == expected
