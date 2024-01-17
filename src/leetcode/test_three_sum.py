563c03002c12e7ccd25f7d9bce413d37a7f76b11
Raymond Sohn <raymondsohn@gmail.com>
2024-01-17 09:59:23 -0800

    Add N^2 solution to 3sum

--- src/leetcode/test_three_sum.py
+++ src/leetcode/test_three_sum.py
@@ -6,7 +6,7 @@ from .. import utils
 
 
 class Solution(utils.BaseSolution):
-    def brute_force(self, nums: List[int]) -> List[List[int]]:
+    def cubic(self, nums: List[int]) -> List[List[int]]:
         triplets = set()
         self.complexity.store(triplets)
         for i, a in enumerate(nums):
@@ -16,6 +16,27 @@ class Solution(utils.BaseSolution):
                     if i != j and i != k and j != k:
                         if a + b + c == 0:
                             triplets.add(tuple(sorted([a, b, c])))
+        return [list(t) for t in triplets]
+
+    def quadratic(self, nums: List[int]) -> List[List[int]]:
+        triplets = set()
+        self.complexity.store(triplets)
+        nums.sort()
+        for i in range(len(nums)):
+            target = -nums[i]
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                self.complexity.step()
+                s = nums[j] + nums[k]
+                if s < target:
+                    j += 1
+                elif s > target:
+                    k -= 1
+                else:
+                    triplets.add(tuple(sorted((nums[i], nums[j], nums[k]))))
+                    j += 1
+                    k -= 1
         return [list(t) for t in triplets]
 
 
@@ -34,7 +55,7 @@ def test_solutions():
 
 def test_complexity():
     s = Solution()
-    tests = [range(n) for n in range(10)]
+    tests = [list(range(n)) for n in range(10)]
 
     for name, func in s.methods():
         for nums in tests:
