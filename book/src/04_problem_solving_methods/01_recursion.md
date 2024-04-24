5f734cc3ab78cd338c36ea81a9c174fba945f626
Raymond Sohn <raymondsohn@gmail.com>
2024-04-24 08:03:51 -0700

    Add binary exponentiation example
    Add binary exponentiation example.

--- book/src/04_problem_solving_methods/01_recursion.md
+++ book/src/04_problem_solving_methods/01_recursion.md
@@ -12,21 +12,14 @@ $F_{0} = 0$
 $F_{1} = 1$  
 $F_{n} = F_{n-1}+F_{n-2}\qquad for\ n > 1$
 
-![](images/fibonacci-sequence-recursion.png)
-
-$F_{n} = F_{n-1}+F_{n-2}$  
-$F_{5} = F_{4}+F_{3}$  
-$F_{5} = (F_{3}+F_{2})+(F_{2}+F_{1})$  
-$F_{5} = ((F_{2}+F_{1})+(F_{1}+F_{0}))+((F_{1}+F_{0})+F_{1})$  
-$F_{5} = (((F_{1}+F_{0})+F_{1})+(F_{1}+F_{0}))+((F_{1}+F_{0})+F_{1})$  
-$F_{5} = (((1+0)+1)+(1+0))+((1+0)+1)$  
-$F_{5} = 5$
-
-```typescript
-export function fib(n: number): number {
-    if (n == 0 || n == 1) {
+```python
+def fib(n: int) -> int:
+    if n <= 1:
         return n
-    }
-    return fib(n - 1) + fib(n - 2)
-}
+
+    return fib(n-1) + fib(n-2)
 ```
+
+### Binary Exponentiation
+
+${\displaystyle x^{n}={\begin{cases}x\,(x^{2})^{(n-1)/2},&{\mbox{if }}n{\mbox{ is odd}}\\(x^{2})^{n/2},&{\mbox{if }}n{\mbox{ is even}}\end{cases}}}$
---
+++ src/uncategorized/test_binary_exponentiation.py
@@ -0,0 +1,43 @@
+import math
+
+
+from .. import utils
+
+
+class Solution(utils.BaseSolution):
+    def logarithmic(self, x: int, n: int):
+        self.complexity.step()
+        if n == 0:
+            return 1
+        elif n < 0:
+            return 1 / self.logarithmic(x, -n)
+        elif n % 2 == 0:
+            return self.logarithmic(x * x, n / 2)
+        else:
+            return x * self.logarithmic(x * x, (n - 1) / 2)
+
+
+def test_solutions():
+    s = Solution()
+    tests = [
+        ([2, 0], 1),
+        ([2, 2], 4),
+        ([2, 10], 1024),
+        ([2, -2], 0.25),
+    ]
+
+    for name, func in s.methods():
+        for args, expected in tests:
+            assert func(*args) == expected, f"{name}"
+
+
+def test_complexity():
+    s = Solution()
+    tests = range(1, 10_000)
+
+    for name, func in s.methods():
+        for n in tests:
+            func(2, n)
+            s.complexity.mark(name, n)
+
+    s.complexity.plot()
